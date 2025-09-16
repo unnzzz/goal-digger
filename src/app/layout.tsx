ff00e51b5aeb206857340a86fafe8c02cde83878
ubukhar2 <ubukhar2@ncsu.edu>
2025-09-15 22:24:32 -0400

    Add Google AdSense code to site head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai:wght@400&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7178518721701659"
+                crossOrigin="anonymous"></script>
       </head>
       <body>
         <Provider>
